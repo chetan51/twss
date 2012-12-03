15e7122ef6d443b073b7941572ea24c5ebacef37
Chetan Surpur <chetan51@gmail.com>
2012-12-02 19:26:19 -0800

    Moved test function to the test file

--- twss_classifier.py
+++ twss_classifier.py
@@ -26,18 +26,3 @@ def classify(item):
 
 def probabilities(title):
   return [(classification, classifier.probability(title, classification)) for classification in classifier.classifications()]
-
-def classify_file(file_path, expected_classification):
-  failures = set()
-  success_count = 0
-  total_items = 0
-  for line in lines(file_path):
-    classification = classify(line)
-    if classification == expected_classification: success_count += 1
-    else: failures.add(line)
-    total_items += 1
-  
-  print "Expected classification: " + expected_classification
-  print "Success rate: " + str((100.0 * success_count) / total_items) + "%"
-  print "Failures: "
-  for failure in failures: print "\t" + failure
--- twss_classifier_test.py
+++ twss_classifier_test.py
@@ -1,10 +1,27 @@
 import twss_classifier
+from doc_processor import *
 reload(twss_classifier)
 
+def classify_file(file_path, expected_classification):
+  failures = set()
+  success_count = 0
+  total_items = 0
+  for line in lines(file_path):
+    classification = twss_classifier.classify(line)
+    if classification == expected_classification: success_count += 1
+    else: failures.add(line)
+    total_items += 1
+  
+  print "Expected classification: " + expected_classification
+  print "Success rate: " + str((100.0 * success_count) / total_items) + "%"
+  print "Failures: "
+  for failure in failures: print "\t" + failure
+
+## Main test
 twss_classifier.load()
 
 # print twss_classifier.classify("wow, that's a hard one!")
 # print twss_classifier.classify("the dogs ate the speakers")
 
-twss_classifier.classify_file('dataset/test_twss.txt', 'yes')
-twss_classifier.classify_file('dataset/test_non_twss.txt', 'no')
+classify_file('dataset/test_twss.txt', 'yes')
+classify_file('dataset/test_non_twss.txt', 'no')
