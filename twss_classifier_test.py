7954682a5fb71af77dc475c693f3f554cc7854ff
Chetan Surpur <chetan51@gmail.com>
2012-12-02 19:04:27 -0800

    Fixed code in test

--- twss_classifier_test.py
+++ twss_classifier_test.py
@@ -5,8 +5,8 @@ from twss_classifier import *
 train('dataset/twss.txt', 'yes')
 train('dataset/non_twss.txt', 'no')
 
-classify("wow, that's a hard one!")
-classify("the dogs ate the speakers")
+# print classify("wow, that's a hard one!")
+# print classify("the dogs ate the speakers")
 
 classify_file('dataset/test_twss.txt', 'yes')
 classify_file('dataset/test_non_twss.txt', 'no')
